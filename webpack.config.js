13993b5c0d798889161bb3730321da616d3dbd95
Alisa Chernysheva <chernysheva.alisa@gmail.com>
2020-06-14 22:03:56 +0200

    #module1-tas1: project settings

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require(`path`);
+const publicFolder = path.join(__dirname, `public`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: publicFolder
+  },
+  devServer: {
+    contentBase: publicFolder,
+    open: true,
+    inline: true,
+    port: 1337,
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: `babel-loader`
+      }
+    },
+    ],
+  },
+  devtool: `source-map`
+};
